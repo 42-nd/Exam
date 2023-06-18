2394d61f6a1cd1a50ef07932e46e94eeca079c86
42-nd <rtrt8682@gmail.com>
2023-06-18 17:04:04 +0700

    ticket21

---
+++ ticket21.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct dat
+{
+    float elem;
+    char name[25];
+};
+
+struct tree
+{
+    tree *right;
+    tree *left;
+    dat info;
+};
+
+void addTreeElement(dat dummy, tree *&node)
+{
+    if (node == nullptr)
+    {
+        node = new tree;
+        node->info = dummy;
+        node->left = nullptr;
+        node->right = nullptr;
+    }
+    else if (dummy.elem < node->info.elem)
+    {
+        addTreeElement(dummy, node->left);
+    }
+    else
+    {
+        addTreeElement(dummy, node->right);
+    }
+}
+
+void delTreeMaxElement(tree *&node)
+{
+    if (node->right != nullptr)
+	{
+		delTreeMaxElement(node->right);
+	}
+	else if (node->left == nullptr){
+        node = nullptr;
+    }
+	else if (node->left != nullptr){
+		node = node->left;
+    }
+	else{
+		node->left = node;
+    }
+}
+
+void printTree(tree *&node)
+{
+    if (node != nullptr)
+    {
+        printTree(node->right);
+        printTree(node->left);
+        cout << node->info.name << " " << node->info.elem << endl;
+    }
+}
+
+
+int main()
+{
+    ifstream text("ticket21.txt");
+    tree *root;
+    root = nullptr;
+    dat dummy;
+    while (!text.eof())
+    {
+        text >> dummy.name;
+        text >> dummy.elem;
+        addTreeElement(dummy, root);
+    }
+    printTree(root);
+    cout << "---------------" << endl;
+    delTreeMaxElement(root);
+    printTree(root);
+    cout << "---------------" << endl;
+    delTreeMaxElement(root);
+    printTree(root);
+}
